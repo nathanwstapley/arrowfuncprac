0f99d798ea213ec0d281d160efccde473d5a3a0b
Nathan W. Stapley <nathanwstapley@gmail.com>
2023-05-11 10:02:12 -0400

    finished exercise code

---
+++ app.js
@@ -0,0 +1,25 @@
+/*
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+  }
+
+*/
+
+// refactor this into es6 / ES2015
+let double =  arr => arr.map( val => val * 2)
+
+/*
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+  */
+  
+  const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0);
